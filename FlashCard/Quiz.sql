a9021b965fa26271d585ee470323ec027b0ffafc
Vincent Saelzler <vincent.saelzler@gmail.com>
2017-10-10 00:35:17 -0400

    update quiz to make PersonId optional and add description

--- FlashCard/Quiz.sql
+++ FlashCard/Quiz.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Quiz]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+	[Description] NVARCHAR(255),
     [Started] DATETIME2 NOT NULL, 
     [Finished] DATETIME2 NULL, 
-    [PersonId] INT NOT NULL, 
+    [PersonId] INT NULL, 
     CONSTRAINT [FK_Quiz_Person] FOREIGN KEY (PersonId) REFERENCES [Student]([Id])
 )
