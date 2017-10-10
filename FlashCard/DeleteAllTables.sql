6b6ca60af6a2817f52e0bda397e44f9b0425748e
Vincent Saelzler <vincent.saelzler@gmail.com>
2017-10-10 00:34:31 -0400

    add utility script to delete all data

---
+++ FlashCard/DeleteAllTables.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [Util].[DeleteAllTables]
+AS
+	DELETE FROM dbo.Question
+	DELETE FROM dbo.[Round]
+	DELETE FROM dbo.Quiz
+	DELETE FROM dbo.Student
+	--not sure if I actially need a "GO" statement to first complete the delete transaction
+	--before resetting identity. For now just run this twice.
+	DBCC CHECKIDENT ('Question', RESEED, 1) 
+	DBCC CHECKIDENT ('[Round]', RESEED, 1) 
+	DBCC CHECKIDENT ('Quiz', RESEED, 1) 
+	DBCC CHECKIDENT ('Student', RESEED, 1) 
+RETURN 0
